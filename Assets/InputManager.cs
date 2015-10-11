0167c258cc498dc414db2df7d11daea20ea7381c
Pavel Raschetnov <pasharvs@gmail.com>
2015-10-11 20:22:13 +0300

    add InputManager

---
+++ Assets/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+	public GameObject gridObject;
+
+	private Vector3 dragBeginPos;
+	private const float MoveThreshold = 0.5f;
+
+	void OnMouseDown() {
+		Vector3 mousePos = Input.mousePosition;
+		dragBeginPos = Camera.main.ScreenToWorldPoint(mousePos);
+		Debug.Log(dragBeginPos.x + " " + dragBeginPos.y + " " + dragBeginPos.z);
+	}
+
+	void OnMouseUp() {
+		Vector3 curWorldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 dragVector = curWorldMousePos - dragBeginPos;
+		processDrag(dragVector);
+	}
+
+
+	private void processDrag(Vector3 dragVector) {
+		int col = (int) (dragBeginPos.x + 2);
+		int row = (int) (2 - dragBeginPos.z);
+		Debug.Log (" row: " + row.ToString() + "col: " + col.ToString());
+
+		bool isDragHorizontal = Mathf.Abs(dragVector.x) > Mathf.Abs(dragVector.z);
+		if (isDragHorizontal && (dragVector.x > MoveThreshold)) {
+			gridObject.GetComponent<GridManager>().moveRight(row, col);
+		} else if (isDragHorizontal && (dragVector.x < - MoveThreshold)) {
+			gridObject.GetComponent<GridManager>().moveLeft(row, col);
+		} else if (!isDragHorizontal && (dragVector.z > MoveThreshold)) {
+			gridObject.GetComponent<GridManager>().moveUp(row, col);
+		} else if (!isDragHorizontal && (dragVector.z < - MoveThreshold)) {
+			gridObject.GetComponent<GridManager>().moveDown(row, col);
+		}
+	}
+}
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class GridManager : MonoBehaviour {
 	public GameObject tile;
 	private int[,] grid;
-	private GameObject[] tiles;
+	private GameObject[,] tiles;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +17,11 @@ public class GridManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//comment
+
 	
 	}
+
+
 
 	private void createGrid() {
 		this.grid = new int[4,4];
@@ -54,6 +58,8 @@ public class GridManager : MonoBehaviour {
 
 		for (var i = 0; i < 4; i++) {
 			for (var j = 0; j < 4; j++) {
+				if (grid[i,j] != 0) {
+				}
 				grid [i, j] = ar [i * 4 + j];		
 			}
 		}
@@ -61,23 +67,89 @@ public class GridManager : MonoBehaviour {
 	}
 
 	private void createTiles() {
-		tiles = new GameObject[15];
+		tiles = new GameObject[4,4];
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				if (grid[i,j] != 0)	{
-					var x =  (0.5 + i);
+					var x =  (-1.5 + j);
 					var y = 0.1;
-					var z = -0.5 - j;
+					var z = 1.5 - i;
 					Vector3 position = new Vector3(	(float) x, (float) y, (float) z );	
 					GameObject new_tile =  (GameObject) Instantiate(tile, position, Quaternion.identity);
 					TextMesh textMesh = new_tile.GetComponentInChildren<TextMesh>();
 					textMesh.text = grid[i,j].ToString();
 
-					tiles[i] = new_tile;
+					tiles[i,j] = new_tile;
 				}
 			}
 		}
 
+	}
+
+	public void moveRight(int row, int col) {
+		Debug.Log ("moveright" + row.ToString() + " " + col.ToString());
+		Debug.Log (gridToString());
+		for (int i = 3; i > col; i--) {
+			if (grid[row,i] == 0 && i > 0) {
+				swap(row, i, row, i-1);
+				updateTiles();
+			}
+		}
+		Debug.Log (gridToString());
+	}
+
+	public void moveLeft(int row, int col) {
+		for (int i = 0; i < col; i++) {
+			if (grid[row,i] == 0 && i < 3) {
+				swap (row, i, row, i+1);
+				updateTiles();
+			}
+		}
+	}
+
+	public void moveUp(int row, int col) {
+		for (int j = 0; j < row; j++) {
+			if (grid[j,col] == 0 && j < 3) {
+				swap (j, col, j+1, col);
+				updateTiles();
+
+			}
+		}
+	}
+
+	public void moveDown(int row, int col) {
+		for (int j = 3; j > row; j--) {
+			if (grid[j, col] == 0 && j > 0) {
+				swap (j, col, j-1, col);
+				updateTiles();
+
+			}
+		}
+
+	}
+
+	private void swap(int rowA, int colA, int rowB, int colB) {
+		//swap ints in grid
+		int temp = grid [rowA, colA];
+		grid [rowA, colA] = grid [rowB, colB];
+		grid [rowB, colB] = temp;
+		//swap corresponding tiles 
+		GameObject tempOb = tiles [rowA, colA];
+		tiles [rowA, colA] = tiles [rowB, colB];
+		tiles [rowB, colB] = tempOb;
+	}
+
+	private void updateTiles() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (grid[i,j] != 0)	{
+					var x =  (-1.5 + j);
+					var y = 0.1;
+					var z = 1.5 - i;
+					tiles[i,j].transform.position = new Vector3((float) x, (float) y, (float) z );
+				}
+			}
+		}
 	}
 
 	private string gridToString() {
